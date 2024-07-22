9d32382f4cc64461f46a0fd661f08bfd41993178
Abdo0422 <119792655+Abdo0422@users.noreply.github.com>
2024-07-22 13:51:11 +0100

    New css

--- PHP/index.php
+++ PHP/index.php
@@ -1,16 +1,20 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php');
-    exit;
+$title = 'Home';
+include __DIR__ . '/../Html/header.html';
+
+if (isset($_SESSION['user_id'])) {
+    // User is logged in
+    echo '<h2>Welcome</h2><br />';
+    echo '<p>You are logged in.</p><br />';
+    echo '<a href="logout.php">Logout</a>';
+} else {
+    // User is not logged in
+    echo '<h2>Welcome to the Authentication Example</h2><br />';
+    echo '<a href="login.php" class="button">Login</a>';
+    echo '<a href="register.php" class="button">Register</a>';
 }
 
-$title = 'Welcome';
-include __DIR__ . '/../Html/header.html';
+include __DIR__ . '/../Html/footer.html';
 ?>
-
-<p>Welcome, you are logged in!</p>
-<a href="logout.php">Logout</a>
-
-<?php include __DIR__ . '/../Html/footer.html'; ?>
